e71b745a36d60d132f63f00d152357dfe82779cc
Mauricio Lauffer <lauffer@outlook.com>
2018-12-03 11:45:22 +1100

    Lint improvements Cache control

--- src/webapp/js/gotmoneyapp.js
+++ src/webapp/js/gotmoneyapp.js
@@ -1,3 +1,5 @@
+'use strict';
+
 window.GOTMONEY = {
   FACEBOOK_APP_ID: (location.hostname === 'localhost') ? '542787052549338' : '182002078627839',
   BACKEND_API_HOSTNAME: (location.hostname === 'localhost') ? 'http://localhost:3000' : 'https://gotmoneyapp.herokuapp.com'
@@ -6,7 +8,6 @@ window.GOTMONEY = {
 //GOOGLE
 const Google = {auth2: null}; // The Sign-In object.
 function onLoadGoogleClient() {
-  'use strict';
   gapi.load('auth2', function() {
     Google.auth2 = gapi.auth2.init();
   });
@@ -15,7 +16,6 @@ function onLoadGoogleClient() {
 //GOOGLE ANALYTICS
 window.dataLayer = window.dataLayer || [];
 function gtag() {
-  'use strict';
   window.dataLayer.push(arguments);
 }
 gtag('js', new Date());
@@ -23,7 +23,6 @@ gtag('config', 'UA-11465363-7');
 
 //Facebook SDK
 window.fbAsyncInit = function() {
-  'use strict';
   FB.init({
     appId: window.GOTMONEY.FACEBOOK_APP_ID,
     xfbml: true,
